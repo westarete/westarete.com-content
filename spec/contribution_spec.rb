d331b3c894fedb7acbd3a26663e9f6b358281405
Scott Woods <scott@westarete.com>
2011-12-03 11:57:47 -0500

    Generalize spec for markdown support

--- spec/contribution_spec.rb
+++ spec/contribution_spec.rb
@@ -9,16 +9,5 @@ describe "An instance of Contribution" do
   it { should validate_presence_of :name }
   it { should validate_presence_of :site_url }
   it { should validate_presence_of :description }
-
-  context 'with a description in markdown format' do
-    before do
-      @contribution = Contribution.new
-      @contribution.description_in_markdown = %{
-        This is in *markdown* format.
-      }
-    end
-    it 'should convert it to HTML' do
-      @contribution.description.strip.should == '<p>This is in <em>markdown</em> format.</p>'
-    end
-  end
+  it { should have_markdown_support_for :description }
 end
--- spec/support/matchers/attributes.rb
+++ spec/support/matchers/attributes.rb
@@ -4,3 +4,12 @@ RSpec::Matchers.define :have_an_attribute do |attribute_name|
     object.send(attribute_name) == 'something'
   end
 end
+
+RSpec::Matchers.define :have_markdown_support_for do |attribute_name|
+  match do |object|
+    object.send("#{attribute_name}_in_markdown=", %{
+      This is in *markdown* format.
+    })
+    object.send(attribute_name).strip == '<p>This is in <em>markdown</em> format.</p>'
+  end
+end
