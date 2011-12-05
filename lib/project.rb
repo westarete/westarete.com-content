0ce3489aa28a1338143f02a8ad5098f0e63f04c1
Scott Woods <scott@westarete.com>
2011-12-04 21:35:40 -0500

    Attach services to projects

--- lib/project.rb
+++ lib/project.rb
@@ -2,8 +2,14 @@ require 'uri'
 require 'content_model'
 
 class Project < ContentModel
-  required_attributes :name, :site_url, :description
+  required_attributes :name, :site_url, :description, :names_of_services_provided
   validates_format_of :site_url, :with => URI.regexp
   markdown_support_for :description
   create_id_from :name
+
+  def services_provided
+    @names_of_services_provided.map do |name|
+      Service.find(:name, name)
+    end
+  end
 end
--- spec/project_spec.rb
+++ spec/project_spec.rb
@@ -5,6 +5,7 @@ describe Project do
 
   it { should have_an_attribute :name }
   it { should validate_presence_of :name }
+  it { should have_an_id_based_on(:name) }
 
   it { should have_an_attribute :site_url }
   it { should validate_presence_of :site_url }
@@ -17,6 +18,42 @@ describe Project do
   it { should validate_presence_of :description }
   it { should have_markdown_support_for :description }
 
-  it { should have_an_id_based_on(:name) }
+  it { should have_an_attribute :names_of_services_provided }
+  it { should validate_presence_of :names_of_services_provided }
 
+  describe '#services_provided' do
+    let!(:cms) do
+      Service.create { |s| s.name = 'Content Management Systems' }
+    end
+    let!(:hosting) do
+      Service.create { |s| s.name = 'Hosting and Support' }
+    end
+    let!(:ixd) do
+      Service.create { |s| s.name = 'Interaction Design' }
+    end
+    let(:project) { Project.new }
+    let(:result) { project.services_provided }
+    context 'when all service names are recognized' do
+      before do
+        project.names_of_services_provided = [
+          'Content Management Systems',
+          'Hosting and Support'
+        ]
+      end
+      it 'should return the matching set of services' do
+        result.should == [cms, hosting]
+      end
+    end
+    context 'when one of the service names is not recognized' do
+      before do
+        project.names_of_services_provided = [
+          'Content Management Systems',
+          'Something Unrecognized'
+        ]
+      end
+      it 'should raise a NotFoundError' do
+        expect { result }.to raise_error(NotFoundError)
+      end
+    end
+  end
 end
